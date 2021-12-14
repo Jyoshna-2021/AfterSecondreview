56e5a5a23e61c9a4c4d1af8c1344e3fa4ccda0ca
Jyoshna-2021 <91448597+Jyoshna-2021@users.noreply.github.com>
2021-12-14 17:27:53 +0530

    Delete myapp-master directory

--- myapp-master/src/index.js
+++
@@ -1,7 +0,0 @@
-
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Path from './components/route'
-import '../src/components/css/index.css'
-
-ReactDOM.render(<Path />, document.getElementById('root'))
